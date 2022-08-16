2685584cd9260541a6902391b68ed33e24783849
muhammadsohail <muhammad.sohail@branex.com>
2022-08-16 17:06:00 +0500

    Initial Commit

---
+++ Shared/Firebase_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_AppApp.swift
+//  Shared
+//
+//  Created by Nanosoft on 2022-08-16.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
